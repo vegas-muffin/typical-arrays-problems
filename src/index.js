1b3c47582206e6daa508ecdf0a2a6326a4c33914
Vagiz Musin <vagiz_mus@mail.ru>
2020-09-30 12:29:21 +0600

    feat: add min max avg task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
+exports.min = function min(array) {
+    let c = "";
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else if (array.length > 0) {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]) {
+                    c = array[i];
+                    array[i] = array[j];
+                    array[j] = c;
+                }
+            }
+        }
+    }
+    return array[array.length - 1];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let c = "";
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else if (array.length > 0) {
+        for (let i = 0; i < array.length - 1; i++) {
+            for (let j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    c = array[i];
+                    array[i] = array[j];
+                    array[j] = c;
+                }
+            }
+        }
+    }
+    return array[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let c = 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else if (array.length > 0) {
+        for (let i = 0; i < array.length; i++) {
+            c = c + array[i];
+        }
+        return c / array.length;
+    }
+};
